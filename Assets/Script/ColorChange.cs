cae65c76dd9f743d25915fbf865fcb4f228b9a0a
rodrigomp6 <rodrigomp6@gmail.com>
2021-04-19 21:04:34 -0300

    First version

---
+++ Assets/Script/ColorChange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+    
+{
+    public Texture ODS7;
+    public Texture ODS14;
+    public Material Sky7;
+    public void Red()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+        GetComponent<Renderer>().material.mainTexture = ODS7;
+        RenderSettings.skybox = default;
+    }
+ public void Blue()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+        GetComponent<Renderer>().material.mainTexture = ODS14;
+        RenderSettings.skybox = Sky7;
+    }
+ public void Black()
+    {
+        GetComponent<Renderer>().material.color = Color.black;
+    }
+}
