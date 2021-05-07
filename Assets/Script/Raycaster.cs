bdffea13b7160ff58388165ea58dbe18ff676978
Rodrigo <rodrigomp6@gmail.com>
2021-05-07 15:27:23 -0300

    Temporizador para mudança da imagem nos displays

---
+++ Assets/Script/Raycaster.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Raycaster : MonoBehaviour
+{
+    private static readonly float lookDelay = 3;
+    private float counter = lookDelay;
+
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hit, 6))
+        {
+            if (hit.transform.gameObject.CompareTag("Display"))
+            {
+                GameObject parent = hit.transform.parent.gameObject;
+                if (parent)
+                {
+                    counter -= Time.deltaTime;
+                    if (counter <= 0)
+                    {
+                        parent.transform.Find("Holder").Find("Paint").gameObject.SendMessage("Info1");
+                        counter = lookDelay;
+                    }
+                }
+            }
+            else
+            {
+                counter = lookDelay;
+            }
+        }
+
+    }
+}
