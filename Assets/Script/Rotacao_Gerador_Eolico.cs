5bf12c7c06bde2853dcb105aacae57fc5cf59952
Areinha <areinhaliste@gmail.com>
2021-05-13 21:27:20 -0300

    Cenas adicionadas e objetos atualizados.
    + Cenários para cada tipo de gerador de energia foram adicionados.
    + Script de rotação feito.
    + Modelos ajustados.

---
+++ Assets/Script/Rotacao_Gerador_Eolico.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacao_Gerador_Eolico : MonoBehaviour
+{
+    public bool EixoY;
+    public bool EixoX;
+    public bool EixoZ;
+
+    public float Velocidade = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EixoX == true)
+        {
+            transform.Rotate(Time.deltaTime * Velocidade, 0, 0, Space.Self);
+        }
+
+        if (EixoY == true)
+        {
+            transform.Rotate(0, 0, Time.deltaTime * Velocidade, Space.Self);
+        }
+
+        if (EixoZ == true)
+        {
+            transform.Rotate(0, Time.deltaTime * Velocidade,0, Space.Self);
+        }
+
+    }
+}
