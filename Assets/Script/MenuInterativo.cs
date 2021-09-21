e34d09b41afb3969d2adac7e091e7728b8e508d9
Areinha <areinhaliste@gmail.com>
2021-09-21 14:39:47 -0300

    Criação do menu de sair + tela de créditos.
    Cena "PortaParaSair" foi criada.
    + Créditos na última sala.

---
+++ Assets/Script/MenuInterativo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInterativo : MonoBehaviour
+{
+    public void Sair(string Scene)
+    {
+        SceneManager.LoadScene("PortaParaSair");
+    }
+
+    public void Voltar(string Scene)
+    {
+        SceneManager.LoadScene("Scene");
+    }
+
+    public void DoExitGame()
+    {
+        Application.Quit();
+    }
+
+}
